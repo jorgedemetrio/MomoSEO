718e5cdff75fe7bc1c35bb28471d944dbbff4f74
Jorge Demetrio <jorge.demetrio@alldreams.com.br>
2017-09-30 05:59:58 -0300

    Testes

--- com_momoseo/src/tests/php/bootstrap.php
+++ com_momoseo/src/tests/php/bootstrap.php
@@ -1,11 +1,31 @@
 <?php
 // No direct access to this file
-define('_JEXEC','S');
 
 
-class JControllerLegacy{}
 
-function jimport($lib){
-		
-	
+error_reporting(E_ALL);
+
+define('_JEXEC', 1);
+define('BASEPATH',realpath(dirname(__FILE__).'/../../main/php/com_momoseo'));
+define('JOOMLA_PATH',realpath(dirname(__FILE__).'/../../main/php/'));
+define('JOOMLA_ADMIN_PATH',realpath(dirname(__FILE__).'/../../main/php/'));
+$_SERVER['HTTP_HOST'] = 'localhost';
+$_SERVER['REQUEST_METHOD'] = 'GET';
+
+if (file_exists(JOOMLA_ADMIN_PATH . '/defines.php'))
+{
+	include_once JOOMLA_ADMIN_PATH . '/defines.php';
 }
+
+if (!defined('_JDEFINES'))
+{
+	define('JPATH_BASE', realpath(dirname(__FILE__).'/../../admin/'));
+	require_once JPATH_BASE . '/includes/defines.php';
+}
+
+require_once JPATH_BASE . '/includes/framework.php';
+require_once JPATH_BASE . '/includes/helper.php';
+require_once JPATH_BASE . '/includes/toolbar.php';
+define('JPATH_COMPONENT',JOOMLA_ADMIN_PATH.'/components/com_content');
+$app = JFactory::getApplication('administrator');
+//include BASEPATH.'/controller.php';
