39337c4a2ce08abee4e735c99b871f7e6293aa55
Jorge Demetrio <jorge.demetrio@alldreams.com.br>
2017-10-05 13:26:48 -0300

    Remoção de componentes extras desnecessários

--- com_momoseo/src/main/php/mod_sugestoes_dicas/sql/mysql/install.mysql.utf8.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE IF NOT EXISTS `#__mom_sugestoes` (
-	`id` int(10) PRIMARY KEY AUTO_INCREMENT,
-	`titulo` varchar(50),
-	`texto` varchar(25) NOT NULL,
-
-  PRIMARY KEY (`id`)
-) ENGINE=INNODB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;
--- com_momoseo/src/main/php/mod_sugestoes_dicas/tmpl/default.php
+++
@@ -1,4 +0,0 @@
-<?php 
-// No direct access
-defined('_JEXEC') || die; ?>
-<?php echo $hello; ?>
