60b5b67f1b1a4a5d15b2ab5d25b06c84f477b51d
Jorge Demetrio <jorge.demetrio@alldreams.com.br>
2017-10-05 03:00:10 -0300

    Correção

--- com_momoseo/src/main/php/mod_momo_busca/mod_momo_busca.php
+++ com_momoseo/src/main/php/mod_momo_busca/mod_momo_busca.php
@@ -1,7 +1,9 @@
 <?php
 // No direct access
 defined('_JEXEC') || die;
+
 require_once dirname(__FILE__) . '/helper.php';
+
 $cx = $params->get('cx', 'partner-pub-2442632731348664:3010243382');
 $lblBotaoBusca = $params->get('lblBotaoBusca', 'Busca');
 $lblImagem = $params->get('lblImagem');
